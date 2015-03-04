b13eed8d53537c04f7ac5f1226ead4fcb78dcf32
Steven Buccini <steven@stevenbuccini.com>
2015-03-03 17:23:23 -0800

    Add in Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
+  # We don't need a new user path, becuase users are now created through
+  # devise "sign up" path
+  resources :users, except: [:new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
