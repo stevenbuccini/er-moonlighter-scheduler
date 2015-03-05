99c94c4ffeb542049e73f042ed2199232a6c6340
Steven Buccini <steven@stevenbuccini.com>
2015-03-05 00:51:17 -0800

    Everything is how I like it right now. Scaffolding for admins + doctors generated, debugging registration now.

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :first_name, :last_name, :email
+
+  def init
+  	# Manually set the type to Doctor.
+  	# Every account signed up through Devise starts out as a Doctor,
+  	# and must be manually promoted to an Admin by a current admin.
+  	self.type = :doctor
+  	super
+  end
+end
