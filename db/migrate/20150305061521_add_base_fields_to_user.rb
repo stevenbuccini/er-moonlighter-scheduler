58c7522d2c39fd6c09674b16a23d9cb64e08e22f
Steven Buccini <steven@stevenbuccini.com>
2015-03-05 00:51:17 -0800

    Everything is how I like it right now. Scaffolding for admins + doctors generated, debugging registration now.

---
+++ db/migrate/20150305061521_add_base_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddBaseFieldsToUser < ActiveRecord::Migration
+  def change
+  	# Add basic fields to the User model after Devise does its thing.
+  	 add_column :users, :first_name, :string
+  	 add_column :users, :last_name, :string
+  	 add_column :users, :type, :string
+  end
+end
