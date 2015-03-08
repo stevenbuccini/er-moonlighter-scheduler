e995acdf2a3627f666209f163aef2c94632ad7ec
Steven Buccini <steven@stevenbuccini.com>
2015-03-07 22:43:14 -0800

    Push current stuff to remote branch so people can work on it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Overriding the built-in Devise method to have people edit their info.
+
+
+	# Overriding the built-in Devise method to have people edit their info.
   def after_sign_in_path_for(user)
-  	edit_user_path(current_user.id)
+  	edit_user_registration_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:account_update) << [:email, :first_name, :last_name]
+  end
+
 end
