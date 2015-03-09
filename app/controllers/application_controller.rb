e740191180a615c4d1f147864608423bd5a551d7
Steven Buccini <steven@stevenbuccini.com>
2015-03-08 23:40:49 -0700

    Got STI working, for some reasons the email field is not being saved during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :email
-    devise_parameter_sanitizer.for(:account_update) << [:email, :first_name, :last_name]
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name]
   end
-
 end
