29d03f8d0837f2930f6b1a5d0c935651c7b0c781
Steven Buccini <steven@stevenbuccini.com>
2015-03-09 22:08:55 -0700

    Update documentation.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def build_resource(hash={})
-  	# Force all signups to be doctors
-    # hash[:type] = 'Doctor'
+    # This file was originally here because we were going to override the default registration
+    # process for Devise. However, we decided it would be best to leave these users in limbo
+    # until they are specifically approved by an admin.
+    # However, we were already too far down the rabbit hole in terms of overriding the default
+    # Devise behavior and it's highly likely that we'll be overriding this process later in the 
+    # project, we're leaving this controller here along with all associated views.
   	super(hash)
   end
 end
