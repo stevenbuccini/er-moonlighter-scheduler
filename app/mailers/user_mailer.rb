ef7ae82a729bc0f23951d1c26ca2498ccbe59f31
Alex Triana <alex.triana2@gmail.com>
2015-03-11 19:22:16 -0700

    finished email, using secrets.yml

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,23 @@
 class UserMailer < ApplicationMailer
+	default from: 'er.moonlighter.scheduler.com'
+
+	def send_dummy_emails(doctors)
+		doctors.each do |doctor|
+			dummy_email(doctor)
+		end
+
+	end
+
+	def dummy_email(doctor)
+		@user = doctor
+		mail(to: @user.email, subject: "This is a mass email", text: "Foo bar")
+	end
+
+	
+
+	def welcome_email(user)
+		@user = user
+		@url = 'localhost:3000/login'
+		mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+	end
 end
