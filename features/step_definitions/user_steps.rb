c0b88d5face36d15d0d1a53167721b03ceab5c47
sarafleur <sarafleursultan@gmail.com>
2015-03-13 11:59:44 -0700

    write steps for read_doctors

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,8 +2,29 @@ def add_doctor(first_name, last_name, email, phone_1, phone_2, phone_3)
   FactoryGirl.create(:doctor, :first_name => first_name, :last_name => last_name, :email => email, :phone_1 => phone_1, :phone_2 => phone_2, :phone_3 => phone_3)
 end
 
+def add_admin(email, password)
+  FactoryGirl.create(:admin, :email => email, :password => password)
+end
+
 Given /the following doctors exist/ do |doctors_table|
   doctors_table.hashes.each do |doctor|
-    add_doctor(doctor['first_name'],doctor['last_name'],doctor['email'],doctor['phone_1'],doctor['phone_2'],doctor['phone_3'])
+    add_doctor(doctor['first_name'], doctor['last_name'], doctor['email'], doctor['phone_1'], doctor['phone_2'], doctor['phone_3'])
   end
+end
+
+Given /the following admins exist/ do |admins_table|
+  admins_table.hashes.each do |admin|
+    add_admin(admin['email'], admin['password'])
+  end
+end
+
+Given /one default admin exists/ do
+  add_admin('default@admin.com','password')
+end
+
+Given /I am logged as the default admin/ do
+  visit '/users/login'
+  fill_in 'user_email', :with => 'default@admin.com'
+  fill_in 'user_password', :with => 'password'
+  click_button 'Log in'
 end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,3 +1,11 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
   if page.respond_to? :should
     page.should have_content(text)
