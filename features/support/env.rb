61da18e47ccf000d2243cb5a3933fef4e84483c6
Steven Buccini <steven@stevenbuccini.com>
2015-03-17 16:45:30 -0700

    Track Cucumber test coverage.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,13 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+# Code Coverage stuff
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+require 'coveralls'
+Coveralls.wear!
+
+
 require 'cucumber/rails'
 require 'factory_girl/step_definitions'
 FactoryGirl.find_definitions
