a967250b5fbff2abf94223378cb45e577d096191
sepkristina <sepkristina@berkeley.edu>
2015-03-20 16:33:21 -0700

    admin views with partial and cuke tests

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -2,23 +2,25 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe AdminsController, type: :controller do
-  before do
-     avoids problems with this method being protected
-    ApplicationController.send(:public, *AdminsController.protected_instance_methods)
-  end
 
   describe 'update an admin' do
+    login_admin
     before :each do
-      admin = FactoryGirl.create(:admin, :first_name => "Jennifer", :last_name => "Lopez", :phone_1 => '222-222-2222')
-      admin_params = {:first_name => "Sara", :last_name => "Lopez", :phone_1 => '111-111-1111'}
+      @admin = FactoryGirl.create(:doctor, :first_name => "Jennifer", :last_name => "Lopez", :phone_1 => '222-222-2222')
+      #doctor_params = ActionController::Parameters.new(:first_name => "Suzie", :last_name => "Lopez", :phone_1 => '222-222-2222')
+      #doctor_params = {:first_name => "Sara", :last_name => "Lopez", :phone_1 => '111-111-1111'}
+      @id = Admin.find_by_first_name("Jennifer")
     end
-    it 'should call update in the model' do
-      Admin.should_receive(:update).with(:first_name => "Sara")
-      post :update, {:id => 1, :first_name => "Sara", :last_name => "Lopez", :phone_1 => '111-111-1111'}
-    end
-    it 'should render the confirmation page' do
 
-    end
-    it 'should make the newly updated information available'
+    #it 'should redirect to the doctor page' do
+    #  doctor_params
+    #  put :update, {:id => @id, :doctor => @doctor}
+    #  @doctor.reload
+    #  expect(response).to redirect_to(@doctor)
+    #end
+    #it 'should render the confirmation page' do
+
+    #end
+    #it 'should make the newly updated information available'
   end
 end
