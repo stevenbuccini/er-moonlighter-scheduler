22ba4e2a09531066214b53855abac83841a151c1
alextriana <alex.triana2@gmail.com>
2015-04-02 17:49:25 -0700

    Revert "figaro with local env vars for mandrill email"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,6 @@
 class UserMailer < ApplicationMailer
 	default from: 'er.moonlighter.scheduler.com'
 
-	def mandrill_client
-      @mandrill_client ||= Mandrill::API.new ENV["MANDRILL_APIKEY"]
-    end
-
 	def dummy_email(doctor, subject, text)
 		@user = doctor
 		@text = text
@@ -12,19 +8,8 @@ class UserMailer < ApplicationMailer
 	end
 
 	def welcome_email(user)
-		template_name = "welcome-email"
-        template_content = []
-        message = {
-            to: [{email: user.email}],
-            merge_vars: [
-                {rcpt: user.email,
-                 vars: [
-                     { name: "first_name", content: user.first_name }
-                ]
-                }
-            ]
-        }
-    mandrill_client.messages.send_template template_name, template_content, message 
+		@user = user
+		mail(to: @user.email, subject: 'Welcome to My Awesome Site')
 	end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.active_record.dump_schema_after_migration = false
-    ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
-  }
-  ActionMailer::Base.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "er.moonlighter.scheduler@gmail.com",
+      :password             => ENV["email_password"],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+}
 end
