eab09a3814922450d36a3c5a8e997c8b4e7221b8
Steven Buccini <steven@stevenbuccini.com>
2015-04-03 22:57:25 -0700

    Fix Shift.to_s and updated db/seeds.rb

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,7 +2,7 @@ class Shift < ActiveRecord::Base
 	has_many :doctors
 
 	def to_s
-		self.strftime("%b %e, %I:%M%p - %b %e, %I:%M%p")
+		self.start.strftime("%b %e, %I:%M%p") + " - " +  self.start.strftime("%b %e, %I:%M%p")
 	end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'date'
+
 users = [{:first_name => 'Joe', :last_name => "Biden",:type => "Doctor", :email => 'jbiden@example.com', :phone_1 => '012-345-6789', :phone_2 => '333-333-3333', :phone_3 => '444-444-4444', :password => 11111111},
     	  {:first_name => 'James', :last_name => "Brown",:type => "Doctor", :email => 'jbrown@example.com', :phone_1 => '223-232-2323', :phone_2 => '567-567-5675', :password => 11111111},
         {:first_name => 'Simon', :last_name => "Jones",:type => "Doctor", :email => 'sjones@example.com', :phone_1 => '777-777-7777', :password => 11111111},
@@ -18,10 +20,21 @@ users = [{:first_name => 'Joe', :last_name => "Biden",:type => "Doctor", :email 
         {:first_name => 'Britney', :last_name => "Spears", :type => "User", :email => 'bspears@example.com', :phone_1 => '237-765-9999', :password => 11111111},
         {:first_name => 'Rihanna', :last_name => "Brown", :type => "User", :email => 'missbrown@example.com', :phone_1 => '567-897-8670', :password => 11111111},
         {:first_name => 'Kate', :last_name => "Hudson", :type => "User", :email => 'khudson555@example.com', :phone_1 => '555-678-3467', :password => 11111111}
-  
-  
   	 ]
 
 users.each do |user|
   User.create!(user)
+end
+
+
+shifts = [
+  {start: DateTime.new(2015, 2, 14, 8, 00), end: DateTime.new(2015, 2, 14, 14, 30)},
+  {start: DateTime.new(2015, 6, 28, 20, 00), end: DateTime.new(2015, 6, 29, 8, 30)},
+  {start: DateTime.new(2015, 7, 6, 5, 43), end: DateTime.new(2015, 7, 6, 12, 30)},
+  {start: DateTime.new(2015, 3, 14, 12, 14), end: DateTime.new(2015, 3, 14, 14, 30)},
+  {start: DateTime.new(2015, 5, 3, 2, 50), end: DateTime.new(2015, 6, 2, 15, 30)}
+]
+
+shifts.each do |shift|
+  Shift.create!(shift)
 end
