820ca44aac3c335b1cc084060139b6252bb1d0a8
Alex Triana <alex.triana2@gmail.com>
2015-04-03 23:00:10 -0700

    added br to shifts view

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,7 +2,7 @@ class Shift < ActiveRecord::Base
 	has_many :doctors
 
 	def to_s
-		self.start.strftime("%b %e, %I:%M%p") + " - " +  self.start.strftime("%b %e, %I:%M%p")
+		self.start.strftime("%b %e, %l:%M %p") + " - " +  self.start.strftime("%b %e, %l:%M %p")
 	end
 end
 
