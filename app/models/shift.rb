33a0d15b4e94a0dc67ab8e8bb3d080813bac4e96
Alex Triana <alex.triana2@gmail.com>
2015-04-04 03:52:52 -0700

    finished shifts phase 2

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,8 +1,21 @@
 class Shift < ActiveRecord::Base
-	has_many :doctors
+	belongs_to :doctor
+
+	def init
+		self.confirmed = false
+	end
 
 	def to_s
 		self.start.strftime("%b %e, %l:%M %p") + " - " +  self.start.strftime("%b %e, %l:%M %p")
+	end
+
+	def to_str
+		self.start.strftime("%b %e, %l:%M %p") + " - " +  self.start.strftime("%b %e, %l:%M %p")
+	end
+
+	def book(current_user)
+		self.confirmed = true
+		self.doctor = current_user
 	end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404050110) do
+ActiveRecord::Schema.define(version: 20150404103507) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150404050110) do
   end
 
   create_table "doctors", force: :cascade do |t|
+    t.integer  "shift_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150404050110) do
     t.boolean  "confirmed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "doctor_id"
   end
 
   create_table "users", force: :cascade do |t|
