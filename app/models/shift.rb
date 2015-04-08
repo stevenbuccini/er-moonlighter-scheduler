2ea46570b2c8ca47bca44099954139dee09cd00e
Steven Buccini <steven@stevenbuccini.com>
2015-04-08 15:07:42 -0700

    Add unit tests for Shift Model.

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,6 +3,7 @@ class Shift < ActiveRecord::Base
 
 	after_initialize :init
 
+
 	validates_presence_of :start_datetime, :end_datetime
 	validate :start_datetime_must_be_before_end_datetime
 
@@ -17,11 +18,11 @@ class Shift < ActiveRecord::Base
 	end
 
 	def to_s
-		self.start_datetime.strftime("%b %e, %l:%M %p") + " - " +  self.start_datetime.strftime("%b %e, %l:%M %p")
+		self.start_datetime.strftime("%b %e,%l:%M %p") + " - " +  self.end_datetime.strftime("%b %e,%l:%M %p")
 	end
 
 	def to_str
-		self.start_datetime.strftime("%b %e, %l:%M %p") + " - " +  self.start_datetime.strftime("%b %e, %l:%M %p")
+		self.start_datetime.strftime("%b %e,%l:%M %p") + " - " +  self.end_datetime.strftime("%b %e,%l:%M %p")
 	end
 
 	def book(current_user)
@@ -30,7 +31,10 @@ class Shift < ActiveRecord::Base
 	end
 
 	def start_datetime_must_be_before_end_datetime
-		errors.add(:end_datetime, "can't be before the start time") if start_datetime > end_datetime
+		# Check for non-nil since the ">" operator is not defined for these functions.
+		if start_datetime and end_datetime
+			errors.add(:sanity_check, "end time can't be before the start time") if start_datetime > end_datetime
+		end
 	end
 end
 
---
+++ spec/controllers/shifts_controller_spec.rb
@@ -0,0 +1,3 @@
+# Test the update method
+# Make sure that it handles failures correctly by stubbing out the save method
+# Yeah that's it but it's gonna be harder than you think
