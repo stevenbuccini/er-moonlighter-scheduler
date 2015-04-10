63b88f3d878305d012590365e03062e2ecb7f57c
teegolden <tolukoya@berkeley.edu>
2015-04-09 23:37:09 -0700

    test dashboard index

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe DashboardController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #index" do
+    login_admin
+    it "should returns http success" do
+      user = User.where(:email => "master@example.com")
+      get :index, :id => user
+      expect(response).to redirect_to admins_path
+    end
+  end
+
+
+
 end
