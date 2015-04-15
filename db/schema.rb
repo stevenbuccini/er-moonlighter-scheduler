3bf46055e99a8c50fc4013ea6aba04816e93fcbf
sepkristina <sepkristina@berkeley.edu>
2015-04-14 22:16:04 -0700

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414153457) do
+ActiveRecord::Schema.define(version: 20150405063905) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -34,14 +34,14 @@ ActiveRecord::Schema.define(version: 20150414153457) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20150414153457) do
     t.string   "phone_1"
     t.string   "phone_2"
     t.string   "phone_3"
-    t.boolean  "registration_done",      default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
