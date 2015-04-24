b21c2be5f8f0d97bfd06f7affe351b52e97af201
sepkristina <sepkristina@berkeley.edu>
2015-04-23 20:20:51 -0700

    change seed to avoid nil error

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,9 @@ users = [{:first_name => 'Joe', :last_name => "Biden",:type => "Doctor", :email 
         {:type => "User", :email => 'reg_test@example.com', :password => 11111111}, 
         {:type => "User", :email => 'reg_test2@example.com', :password => 11111111},
         {:type => "User", :email => 'reg_test3@example.com', :password => 11111111},
-        {:type => "Admin", :email => 'admin@example.com', :password => 11111111}, 
+        #{:type => "Admin", :email => 'admin@example.com', :password => 11111111}, 
         #{:type => "Doctor", :email => 'doctor@example.com', :password => 11111111},
-        {:type => "User", :email => 'user@example.com', :password => 11111111}
+        {:type => nil, :email => 'user@example.com', :password => 11111111}
 
   	 ]
 
