063ee2ac5929911c4b38ed1caf60ffd222112ca6
sarafleur <sarafleursultan@gmail.com>
2015-05-02 16:09:43 -0700

    try to fix rspec

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -25,16 +25,15 @@ RSpec.describe AdminsController, type: :controller do
     it 'should redirect to the admin page' do
       put :update, :id => @admin.id, :admin => {:first_name => @new_first_name}
       expect(response).to redirect_to @admin
-    end 
+    end
   end
 
   describe 'sending an email' do
     login_admin
     before :each do
       @doctor = FactoryGirl.create(:doctor)
-      @admin = FactoryGirl.build(:admin, :first_name => "Jennifer", :last_name => "Lopez", :phone_1 => '222-222-2222')
-      @admin.save!
-      @new_first_name = "Sara"
+      @admin = FactoryGirl.create(:admin, :first_name => "Jennifer", :last_name => "Lopez", :phone_1 => '222-222-2222')
+
     end
 
     it "renders the create_email template" do
@@ -43,19 +42,19 @@ RSpec.describe AdminsController, type: :controller do
     end
 
     it "post send_mail redirects to root" do
-      post :send_email, {activated: [@doctor.id]}
+      post :send_email, :activated=> @doctor.id
       expect(response).to redirect_to root_url
     end
   end
 
-  describe 'index method' do 
+  describe 'index method' do
     login_admin
-    before :each do 
+    before :each do
       @admin = FactoryGirl.create(:admin)
       @doctor = FactoryGirl.create(:doctor)
       @shift = FactoryGirl.create(:shift)
     end
-    it 'should call .all on each model' do 
+    it 'should call .all on each model' do
       expect(Admin).to receive(:all).and_return(@admin)
       expect(Doctor).to receive(:all).and_return(@doctor)
       expect(Shift).to receive(:all).and_return(@shift)
@@ -63,56 +62,56 @@ RSpec.describe AdminsController, type: :controller do
     end
   end
 
-  describe 'show' do 
+  describe 'show' do
     login_admin
-    before :each do 
+    before :each do
       @admin = FactoryGirl.create(:admin)
     end
-    it 'should call .find in Admin' do 
+    it 'should call .find in Admin' do
       expect(Admin).to receive(:find).at_least(:once).with(@admin.id.to_s).and_return(@admin)
       get :show, :id => @admin.id
     end
-    it 'should render the show page' do 
+    it 'should render the show page' do
       expect(Admin).to receive(:find).at_least(:once).and_return(@admin)
       get :show, :id => @admin.id
       expect(response).to render_template('admins/show/')
     end
   end
- 
-  describe 'new' do 
+
+  describe 'new' do
     login_admin
-    it 'should call new in Admin' do 
+    it 'should call new in Admin' do
       expect(Admin).to receive(:new)
       get :new
     end
   end
 
-  describe 'edit' do 
+  describe 'edit' do
     login_admin
-    before :each do 
+    before :each do
       @admin = FactoryGirl.create(:admin)
     end
-    it 'should call .find in Admin' do 
+    it 'should call .find in Admin' do
       expect(Admin).to receive(:find).at_least(:once).with(@admin.id.to_s).and_return(@admin)
       get :edit, :id => @admin.id
     end
-    it 'should render the show page' do 
+    it 'should render the show page' do
       expect(Admin).to receive(:find).at_least(:once).and_return(@admin)
       get :edit, :id => @admin.id
       expect(response).to render_template('admins/edit/')
     end
   end
 
-  # describe '#approve_doctor' do 
+  # describe '#approve_doctor' do
   #   login_admin
-  #   before :each do 
+  #   before :each do
   #     @user = FactoryGirl.create(:user)
   #   end
-  #   it 'should find the user' do 
+  #   it 'should find the user' do
   #     expect(User).to receive(:find_by_id)
   #     get :approve_doctor, @user
   #   end
-  #   it 'should call update attributes on the user' do 
+  #   it 'should call update attributes on the user' do
   #     @user.reload
   #     User.stub(:find_by_id).and_return(@user)
   #     expect(@user).to receive(:update_attributes)
@@ -120,25 +119,3 @@ RSpec.describe AdminsController, type: :controller do
   #   end
   # end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
