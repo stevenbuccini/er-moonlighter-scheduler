b4a5ac7096461ea9b99e6a7f97dfc213e5dba2af
Alex Triana <alex.triana2@gmail.com>
2015-05-02 22:24:34 -0700

    added phase to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426202204) do
+ActiveRecord::Schema.define(version: 20150503051646) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150426202204) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "is_open",    default: false
+    t.string   "phase",      default: "1"
   end
 
   create_table "shifts", force: :cascade do |t|
