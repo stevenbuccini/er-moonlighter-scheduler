dbb41b224d3002894fe01c562c04bca4d422b7e2
teegolden <tolukoya@berkeley.edu>
2015-05-05 06:24:40 -0700

    admin assistant Tonia talked about

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503070827) do
+ActiveRecord::Schema.define(version: 20150505131902) do
+
+  create_table "admin_assistants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
