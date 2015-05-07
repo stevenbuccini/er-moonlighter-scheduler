b043c4f6dd1abf77c70ce0de15ed907c929ba694
teegolden <tolukoya@berkeley.edu>
2015-05-06 18:50:31 -0700

    fixed admin assist view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
 		if current_user.type
 	  	if current_user.type == Doctor.NAME 
 				redirect_to :controller => 'doctors', :action => 'index'
-			elsif current_user.type == Admin.NAME or current_user.type == "AdminAssistant"
+			elsif current_user.type == Admin.NAME or current_user.type == "AdminAssistant" 
 				redirect_to :controller => 'admins', :action => 'index'
 			# elsif current_user.type == "AdminAssistant"
 			# 	redirect_to :controller => 'admin_assistant', :action => 'index'
